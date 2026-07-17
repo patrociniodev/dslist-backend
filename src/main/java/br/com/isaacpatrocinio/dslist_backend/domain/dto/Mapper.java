367d08d781fdf4d6ed5277dc7d1289195d9afc88
Isaac Patrocinio <patrociniodev@gmail.com>
2026-07-17 16:36:46 -0300

    Refactor Mapper auxiliary methods

--- src/main/java/br/com/isaacpatrocinio/dslist_backend/domain/dto/Mapper.java
+++ src/main/java/br/com/isaacpatrocinio/dslist_backend/domain/dto/Mapper.java
@@ -1,16 +1,19 @@
 package br.com.isaacpatrocinio.dslist_backend.domain.dto;
 
 import br.com.isaacpatrocinio.dslist_backend.domain.entities.Game;
+import org.springframework.beans.BeanUtils;
 
 public class Mapper {
 
-    public static GameMinDTO toDTO(Game entity) {
-        return new GameMinDTO(
-                entity.getId(),
-                entity.getTitle(),
-                entity.getYear(),
-                entity.getImgUrl(),
-                entity.getShortDescription()
-        );
+    public static GameMinDTO toMinDTO(Game entity) {
+        GameMinDTO minDTO = new GameMinDTO();
+        BeanUtils.copyProperties(entity, minDTO);
+        return minDTO;
+    }
+
+    public static GameDTO toDTO(Game entity) {
+        GameDTO gameDTO = new GameDTO();
+        BeanUtils.copyProperties(entity, gameDTO);
+        return gameDTO;
     }
 }
